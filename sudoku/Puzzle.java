f4d85ad1c97482a708b8f6de7de0895622f404e9
Ervina <162418664+ervinaanggraini@users.noreply.github.com>
2024-12-06 16:00:30 +0700

    Update Puzzle.java

--- sudoku/Puzzle.java
+++ sudoku/Puzzle.java
@@ -1,3 +1,14 @@
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #1
+ * 1 - 5026231078 - Hilman Mumtaz Sya`bani
+ * 2 - 5026231101 - Muhammad Akmal Rafiansyah
+ * 3 - 5026231042 - Ervina Anggraini
+ */
+
+
 package sudoku;
 
 import java.util.Random;

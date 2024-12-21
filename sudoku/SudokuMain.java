e10f4f94f48f612f7cea744884ec44fb6187b4ac
mumtazsya <mumtazsyab@gmail.com>
2024-12-21 21:50:38 +0700

    Update SudokuMain.java

--- sudoku/SudokuMain.java
+++ sudoku/SudokuMain.java
@@ -88,7 +88,11 @@ public class SudokuMain extends JFrame {
         startTime = System.currentTimeMillis(); 
         gameTimer.start(); 
         
-        
+        // Hentikan timer saat puzzle selesai
+        if (board.isSolved()) {
+            gameTimer.stop(); 
+            JOptionPane.showMessageDialog(this, "Congratulations! You solved the puzzle in " + timerLabel.getText()); 
+        }
     }
 
     // Method to reset the game and clear user inputs
@@ -134,12 +138,6 @@ public class SudokuMain extends JFrame {
 
         // Perbarui teks pada label timer
         timerLabel.setText(String.format("Time: %02d:%02d:%02d", hours, minutes, seconds));
-    }
-    
-    // Hentikan timer saat puzzle selesai
-    if (board.isSolved()) {
-        gameTimer.stop(); 
-        JOptionPane.showMessageDialog(this, "Congratulations! You solved the puzzle in " + timerLabel.getText()); 
     }
 }
 
